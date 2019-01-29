48385fd4e793ba434933866d13cdcdbd65231a70
Jim Nervall <nervall@hotmail.com>
2019-01-29 18:03:44 +0100

    update delete

--- script.js
+++ script.js
@@ -2,24 +2,31 @@ let cardList = document.querySelector('#list1');
 let cardButton = document.querySelector('#card1');
 let listButton = document.querySelector('#addList');
 let section = document.querySelector('section');
+let deleteItem = document.querySelector('.material-icons');
 let counter = 1;
 
 cardButton.addEventListener('click', addCard);
 listButton.addEventListener('click', addList);
+deleteItem.addEventListener('click', removeListItem);
 
 function addCard(e) {
     let newItem = document.createElement('li');
     let textArea = document.createElement('textarea');
     textArea.setAttribute('class', '.list-items textarea');
-    textArea.setAttribute('placeholder', 'Skriv din text här...')
+    textArea.setAttribute('placeholder', 'Skriv din text här...');
+    let i = document.createElement('i');
+    //iText = '<i class="material-icons">clear</i>';
+    i.setAttribute('class', 'material-icons');
+    i.textContent = 'clear';
     //newItem.setAttribute('contenteditable', 'true');
     //newItem.textContent = 'Skriv din text här...';
     newItem.appendChild(textArea);
+    newItem.appendChild(i);
     cardList.appendChild(newItem);
 } 
 
 function removeListItem(){
-    //lst.removeChild(this.parentNode);
+    list1.removeChild(this.parentNode);
 }
 
 function addList(e) {
@@ -31,6 +38,11 @@ function addList(e) {
     let textArea = document.createElement('textarea');
     textArea.setAttribute('class', '.list-items textarea');
     textArea.setAttribute('placeholder', 'Skriv din text här...')
+    let i = document.createElement('i');
+    //iText = '<i class="material-icons">clear</i>';
+    i.setAttribute('class', 'material-icons');
+    i.textContent = 'clear';
+    //i.innerHTML = iText;
     //newItem.setAttribute('contenteditable', 'true');
     //newItem.textContent = 'Skriv din text här...';
     let button = document.createElement('button');
@@ -46,9 +58,9 @@ function addList(e) {
     newList.appendChild(ul);
     ul.appendChild(newItem);
     newItem.appendChild(textArea);
+    newItem.appendChild(i);
     newList.appendChild(button);
-
 }
 
-
+//<i class="material-icons" id="delete">clear</i>
 
