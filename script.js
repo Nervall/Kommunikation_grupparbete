d54b8636d1147c2bda0d1e7cbb62a5d49ef52fc8
Jim Nervall <nervall@hotmail.com>
2019-02-04 12:30:57 +0100

    update

--- script.js
+++ script.js
@@ -1,42 +1,82 @@
 let cardList = document.querySelector('#list1');
-console.log(cardList);
-//let parentId = document.querySelectorAll('.list-items');
-let cardButton = document.querySelectorAll('.add-card-btn');
-console.log(cardButton);
 let listButton = document.querySelector('#addList');
 let section = document.querySelector('section');
-let deleteItem = document.querySelector('.material-icons');
-let counter = 1;
 
-if (cardButton === null) {
-    cardButton = document.querySelector('.add-card-btn');
-    cardButton.addEventListener('click', addCard)
-} else {
-    for (let i = 0; i < cardButton.length; i++) {
-        cardButton[i].addEventListener('click', addCard);
-    }
+listButton.addEventListener('click', addList);
+
+let cardButton = document.querySelectorAll('.add-card-btn');
+for (let i = 0; i < cardButton.length; i++) {
+    cardButton[i].addEventListener('click', addCard);
 }
 
-//cardButton.addEventListener('click', addCard);
-listButton.addEventListener('click', addList);
-deleteItem.addEventListener('click', removeListItem);
+let deleteItem = document.querySelectorAll('.material-icons');
+for (let i = 0; i < deleteItem.length; i++) {
+    deleteItem[i].addEventListener('click', removeCard, false);
+}
+
+let counter = 1;
 
 function addCard(e) {
+    let target = e.target
+    console.log(target);
     let newItem = document.createElement('li');
     let textArea = document.createElement('textarea');
     textArea.setAttribute('class', '.list-items textarea');
     textArea.setAttribute('placeholder', 'Skriv din text här...');
-    let i = document.createElement('i');
-    i.setAttribute('class', 'material-icons');
-    i.textContent = 'clear';
+    let iElem = document.createElement('i');
+    iElem.setAttribute('class', 'material-icons');
+    iElem.textContent = 'clear';
+    iElem.addEventListener('click', removeCard);
     newItem.appendChild(textArea);
-    newItem.appendChild(i);
+    newItem.appendChild(iElem);
     cardList.appendChild(newItem);
 } 
 
-function testAddCard(parentId, elementId) {
-
+function removeCard(e){
+    let target = e.target;
+    let li = document.querySelectorAll('li');
+    for (let i = 0; i < li.length; i++) {
+        target.parentNode.remove(this);
+    }
 }
+
+function addList(e) {
+    counter ++;
+    let newList = document.createElement('div');
+    let h3 = document.createElement('h3');
+    let ul = document.createElement('ul');
+    let newItem = document.createElement('li');
+    let textArea = document.createElement('textarea');
+    textArea.setAttribute('class', '.list-items textarea');
+    textArea.setAttribute('placeholder', 'Skriv din text här...')
+    let iElem = document.createElement('ielem');
+    //iText = '<i class="material-icons">clear</i>';
+    iElem.setAttribute('class', 'material-icons');
+    iElem.textContent = 'clear';
+    iElem.addEventListener('click', removeCard);
+    //i.innerHTML = iText;
+    //newItem.setAttribute('contenteditable', 'true');
+    //newItem.textContent = 'Skriv din text här...';
+    let button = document.createElement('button');
+    newList.setAttribute('class', 'list');
+    h3.setAttribute('class', 'list-title');
+    h3.setAttribute('contenteditable', 'true');
+    ul.setAttribute('class', 'list-items');
+    button.setAttribute('class', 'add-card-btn btn');
+    button.setAttribute('id', 'card' + counter);
+    button.addEventListener('click', addCard);
+    h3.textContent = 'En ny lista';
+    button.textContent = '+ Lägg till ett kort';
+    section.insertBefore(newList, listButton);
+    newList.appendChild(h3);
+    newList.appendChild(ul);
+    ul.appendChild(newItem);
+    newItem.appendChild(textArea);
+    newItem.appendChild(iElem);
+    newList.appendChild(button);
+}
+
+
 
 /*
 function addElement(parentId, elementTag, elementId, html) {
@@ -50,42 +90,9 @@ function addElement(parentId, elementTag, elementId, html) {
 
 */
 
-function removeListItem(){
-    cardList.removeChild(this.parentNode);
-}
 
-function addList(e) {
-    counter ++;
-    let newList = document.createElement('div');
-    let h3 = document.createElement('h3');
-    let ul = document.createElement('ul');
-    let newItem = document.createElement('li');
-    let textArea = document.createElement('textarea');
-    textArea.setAttribute('class', '.list-items textarea');
-    textArea.setAttribute('placeholder', 'Skriv din text här...')
-    let i = document.createElement('i');
-    //iText = '<i class="material-icons">clear</i>';
-    i.setAttribute('class', 'material-icons');
-    i.textContent = 'clear';
-    //i.innerHTML = iText;
-    //newItem.setAttribute('contenteditable', 'true');
-    //newItem.textContent = 'Skriv din text här...';
-    let button = document.createElement('button');
-    newList.setAttribute('class', 'list');
-    h3.setAttribute('class', 'list-title');
-    ul.setAttribute('class', 'list-items');
-    button.setAttribute('class', 'add-card-btn btn');
-    button.setAttribute('id', 'card' + counter);
-    h3.textContent = 'En ny lista';
-    button.textContent = '+ Lägg till ett kort';
-    section.insertBefore(newList, listButton);
-    newList.appendChild(h3);
-    newList.appendChild(ul);
-    ul.appendChild(newItem);
-    newItem.appendChild(textArea);
-    newItem.appendChild(i);
-    newList.appendChild(button);
-}
+
+
 
 //<i class="material-icons" id="delete">clear</i>
 
