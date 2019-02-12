052c08056e37f6d3a0e154a03b8ba6bb168a8b01
Flashkeys <43600429+Flashkeys@users.noreply.github.com>
2019-02-12 21:12:59 +0100

    Added popup

--- script.js
+++ script.js
@@ -3,52 +3,55 @@ let listCounter = 0;
 let iForward
 let iBack;
 
+
 let createButton = document.querySelector('#createList');
-createButton.addEventListener('click', addList);
+createButton.addEventListener('click', (e) => {
+    addList(e);
+});
 
-var dragged;
+let dragged;
 
-  document.addEventListener("drag", function( event ) {
-      event.target.style.border = "1px dashed grey"
-  }, false);
+document.addEventListener("drag", function (event) {
+    event.target.style.border = "1px dashed grey"
+}, false);
 
-  document.addEventListener("dragstart", function( event ) {
-      dragged = event.target;
-      event.target.style.opacity = .5;
-  }, false);
+document.addEventListener("dragstart", function (event) {
+    dragged = event.target;
+    event.target.style.opacity = .5;
+}, false);
 
-  document.addEventListener("dragend", function( event ) {
-      event.target.style = "";
-  }, false);
+document.addEventListener("dragend", function (event) {
+    event.target.style = "";
+}, false);
 
-  document.addEventListener("dragover", function( event ) {
-      event.preventDefault();
-  }, false);
+document.addEventListener("dragover", function (event) {
+    event.preventDefault();
+}, false);
 
-  document.addEventListener("dragenter", function( event ) {
-      if ( event.target.className === "list-items" ) {
-          event.target.style.background = "lightgrey";
-      }
-  }, false);
+document.addEventListener("dragenter", function (event) {
+    if (event.target.className === "list-items") {
+        event.target.style.background = "lightgrey";
+    }
+}, false);
 
-  document.addEventListener("dragleave", function( event ) {
-      if ( event.target.className === "list-items" ) {
-          event.target.style.background = "";
-      }
-  }, false);
+document.addEventListener("dragleave", function (event) {
+    if (event.target.className === "list-items") {
+        event.target.style.background = "";
+    }
+}, false);
 
-  document.addEventListener("drop", function( event ) {
-      event.preventDefault();
-      if ( event.target.className === "list-items" ) {
-          event.target.style.background = "";
-          dragged.parentNode.removeChild( dragged );
-          event.target.appendChild( dragged );
-      }
-  }, false);
+document.addEventListener("drop", function (event) {
+    event.preventDefault();
+    if (event.target.className === "list-items") {
+        event.target.style.background = "";
+        dragged.parentNode.removeChild(dragged);
+        event.target.appendChild(dragged);
+    }
+}, false);
 
 function addList(e) {
-    counter ++;
-    listCounter ++;
+    counter++;
+    listCounter++;
     let section = document.querySelector('section');
     let newList = document.createElement('div');
     newList.setAttribute('id', 'div' + counter);
@@ -64,7 +67,15 @@ function addList(e) {
     let iClear = document.createElement('i');
     iClear.setAttribute('class', 'material-icons');
     iClear.textContent = 'clear';
-    iClear.addEventListener('click', removeCard);
+    iClear.addEventListener('click', () => {
+        li.remove();
+    });
+
+    let timedatecard = document.createElement("p");
+    let now = new Date();
+    timedatecard.innerHTML = now.toLocaleString('sv-SE');
+    timedatecard.setAttribute('class', 'timetagcard');
+
     let addCardButton = document.createElement('button');
     h3.setAttribute('class', 'list-title');
     h3.setAttribute('contenteditable', 'true');
@@ -72,17 +83,71 @@ function addList(e) {
     ul.setAttribute('id', 'list' + counter);
     addCardButton.setAttribute('class', 'add-card-btn btn');
     addCardButton.setAttribute('id', 'card' + counter);
-    addCardButton.addEventListener('click', function() {
-        addCard(ul);
+    addCardButton.addEventListener('click', function (e) {
+
+        let section = document.querySelector('section');
+        const container = document.createElement("div");
+        const title = document.createElement("h3");
+        title.innerHTML = 'Create new card'
+        const div = document.createElement("div");
+        div.setAttribute('class', 'popup');
+        div.appendChild(title);
+        const button = document.createElement("button");
+        button.innerHTML = 'Add new item'
+        let textArea = document.createElement('textarea');
+        textArea.setAttribute('class', 'textpop');
+        textArea.setAttribute('placeholder', 'Skriv din text här...');
+        section.appendChild(container);
+        container.setAttribute('class', 'overlay');
+        container.appendChild(div);
+        div.appendChild(textArea);
+        div.appendChild(button);
+
+        container.style.display = 'flex';
+        div.addEventListener('click', (e) => {
+            e.stopPropagation();
         });
+
+        button.addEventListener('click', () => {
+            const value = textArea.value;
+            if (!value) {
+                alert('please enter a value')
+            } else {
+                addCard(ul, value);
+                div.remove();
+                container.style.display = 'none';
+            }
+        })
+
+        textArea.addEventListener('keypress', (key) => {
+            if (key.code === 'Enter') {
+                const value = textArea.value;
+                if (!value) {
+                    alert('please enter a value')
+                } else {
+                    addCard(ul, value);
+                    div.remove();
+                    container.style.display = 'none';
+                }
+            }
+        });
+
+        container.addEventListener('click', (e) => {
+            div.remove();
+            container.style.display = 'none';
+        });
+
+    });
     let addListButton = document.createElement('button');
     addListButton.setAttribute('class', 'add-list-btn');
     addListButton.setAttribute('id', 'listButton' + counter);
     addListButton.textContent = 'Lägg till en lista';
-    addListButton.addEventListener('click', function() {
+    addListButton.addEventListener('click', function () {
         section.removeChild(addListButton);
         addList();
-        });
+    });
+    const licontainer = document.createElement("div");
+    licontainer.setAttribute('class', 'licontainer');
     h3.textContent = 'En ny lista';
     addCardButton.textContent = '+ Lägg till ett kort';
     section.appendChild(newList);
@@ -90,32 +155,37 @@ function addList(e) {
     newList.appendChild(ul);
     ul.appendChild(li);
     li.appendChild(textArea);
-    li.appendChild(iClear);
-    newList.appendChild(addCardButton); 
+    li.appendChild(licontainer);
+    licontainer.appendChild(iClear);
+    licontainer.appendChild(timedatecard);
+    newList.appendChild(addCardButton);
     section.appendChild(addListButton);
 }
 
-function addCard(list) {
-    listCounter ++;
+function addCard(list, value) {
+    listCounter++;
     let ul = list;
     let li = document.createElement('li');
     li.setAttribute('id', 'list-item' + listCounter);
     li.setAttribute('draggable', 'true');
     let textArea = document.createElement('textarea');
     textArea.setAttribute('class', '.list-items textarea');
-    textArea.setAttribute('placeholder', 'Skriv din text här...');  
+    textArea.setAttribute('placeholder', 'Skriv din text här...');
     let iClear = document.createElement('i');
     iClear.setAttribute('class', 'material-icons');
     iClear.textContent = 'clear';
-    iClear.addEventListener('click', removeCard);
+    iClear.addEventListener('click', () => {
+        li.remove();
+    });
+
+    let timedatecard = document.createElement("p");
+    let now = new Date();
+    timedatecard.innerHTML = now.toLocaleString('sv-SE');
+    timedatecard.setAttribute('class', 'timetagcard');
+
+    textArea.value = value;
     li.appendChild(textArea);
     li.appendChild(iClear);
-    ul.appendChild(li); 
+    li.appendChild(timedatecard);
+    ul.appendChild(li);
 } 
-
-function removeCard(e){
-    let target = e.target;
-    document.querySelector('li');
-    target.parentNode.remove(this);
-}
-
